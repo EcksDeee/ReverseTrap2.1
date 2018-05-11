f3ee666fafa194b9ff18096cbfe289b2407d3d2d
TS_Panda <tspandaa@gmail.com>
2018-05-11 00:01:35 -0700

    dev

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ client.on('message', msg => {
         } else if (command === 'noscope') {
             msg.channel.send("", {file: "https://media2.giphy.com/media/UJLtz0z7VrAIM/200.gif"});
         } else if (command === 'help') {
-            msg.channel.send("ReverseTrap [Owner: F1R3SHAD0W]\nBeta\n:>help: Lists possible commands\n:>ping: Says pong..What did u expect??\n:>gifhelp: Lists possible gifs\n:>status: OML THAT TOOK FOREVER USE WISELY")
+            msg.channel.send("ReverseTrap [Owner: F1R3SHAD0W]\nBeta\n:>help: Lists possible commands\n:>ping: Says pong..What did u expect??\n:>gifhelp: Lists possible gifs\n:>status: OML THAT TOOK FOREVER USE WISELY");
         } else if (command === 'stickman') {
             msg.channel.send("", {file: "https://media0.giphy.com/media/jSXkVsYaQxw6k/200.gif"});
         } else if (command === 'rekt') {
@@ -30,44 +30,43 @@ client.on('message', msg => {
         } else if (command === 'ganked') {
             msg.channel.send("", {file: "https://i.gifer.com/U3Wn.gif"});
         } else if (command === 'gifhelp') {
-            msg.channel.send(":>noscope: Noscopes kids\n:>stickman: Stick people beat each other up\n:>rekt: Use ONLY when someone gets toasted\n:>ganked: Partying gets you beat up")
+            msg.channel.send(":>noscope: Noscopes kids\n:>stickman: Stick people beat each other up\n:>rekt: Use ONLY when someone gets toasted\n:>ganked: Partying gets you beat up");
+		}
+	}
         
-     if (pref === config.prefix) {
- 		     if (command === 'enable') {
- 				                     if (botstatus === false) {
- 					                           botstatus = true;
- 					                           msg.channel.send(':white_check_mark: ReverseTrap is now **ENABLED**');
- 				                     } else if (botstatus === true) {
- 					                           msg.channel.send(':white_check_mark: ReverseTrap is already **ENABLED**');
- 				                     } else {
- 					                           return;
- 				                     }
- 			               } else {
- 				                     msg.channel.send(nopermserror);
- 			               }
- 		         } else if (command === 'disable') {
- 				                     if (botstatus === true) {
- 					                           botstatus = false;
- 					                           msg.channel.send(':x: ReverseTrap is now **DISABLED**');
- 				                     } else if (botstatus === false) {
- 					                           msg.channel.send(':x: ReverseTrap is already **DISABLED**');
- 				                     } else {
- 					                           return;
- 				                     }
- 			               } else {
- 				                     msg.channel.send(nopermserror);
- 			               }
-             } else if (command === 'status') {
- 		          	     if (botstatus === true) {
- 				                     msg.channel.send('ReverseTrap is currently: **ENABLED**');
- 			                } else if (botstatus === false) {
- 				                      msg.channel.send('ReverseTrap is currently: **DISABLED**');
- 			                } else {
- 				                      return;
- 		  	              }
-                  } else {
-                      return;
-                  }
-          });
+	if (pref === config.prefix) {
+		if (command === 'enable') {
+			if (botstatus === false) {
+				botstatus = true;
+				msg.channel.send(':white_check_mark: ReverseTrap is now **ENABLED**');
+			} else if (botstatus === true) {
+				msg.channel.send(':white_check_mark: ReverseTrap is already **ENABLED**');
+			} else {
+				return;
+			}
+		} else if (command === 'disable') {
+			if (botstatus === true) {
+				botstatus = false;
+				msg.channel.send(':x: ReverseTrap is now **DISABLED**');
+			} else if (botstatus === false) {
+				msg.channel.send(':x: ReverseTrap is already **DISABLED**');
+			} else {
+				return;
+			}
+		} else if (command === 'status') {
+			if (botstatus === true) {
+				msg.channel.send('ReverseTrap is currently: **ENABLED**');
+			} else if (botstatus === false) {
+				msg.channel.send('ReverseTrap is currently: **DISABLED**');
+			} else {
+				return;
+			}
+		} else {
+			return;
+		}
+	} else {
+		return;
+	}
+});
 
 client.login(config.token);
