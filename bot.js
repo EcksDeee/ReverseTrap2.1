c2f0ba19ec06017de9cbb8d179723cf561f30f7f
EcksDeee <36055694+EcksDeee@users.noreply.github.com>
2018-05-10 22:41:42 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,11 +22,15 @@ client.on('message', msg => {
         } else if (command === 'noscope') {
             msg.channel.send("", {file: "https://media2.giphy.com/media/UJLtz0z7VrAIM/200.gif"});
         } else if (command === 'help') {
-            msg.channel.send("ReverseTrap [Owner: F1R3SHAD0W]\nBeta\n:>help: Lists possible commands\n:>ping: Says pong..What did u expect??\n:>noscope: Noscopes kids\n:>stickman: Stick man eats everyone\n:>rekt: Use ONLY when someone gets toasted")
+            msg.channel.send("ReverseTrap [Owner: F1R3SHAD0W]\nBeta\n:>help: Lists possible commands\n:>ping: Says pong..What did u expect??\n:>gifhelp: Lists possible gifs")
         } else if (command === 'stickman') {
             msg.channel.send("", {file: "https://media0.giphy.com/media/jSXkVsYaQxw6k/200.gif"});
         } else if (command === 'rekt') {
             msg.channel.send("rekt m8", {file: "http://i.imgur.com/olx0xRF.gif"});
+        } else if (command === 'ganked') {
+            msg.channel.send("", {file: "https://i.gifer.com/U3Wn.gif"});
+        } else if (command === 'gifhelp') {
+            msg.channel.send(":>noscope: Noscopes kids\n:>stickman: Stick people beat each other up\n:>rekt: Use ONLY when someone gets toasted\n:>ganked: Partying gets you beat up")
         } else {
             return;
         }
