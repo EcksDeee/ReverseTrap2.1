ec57584f8bb744d1576f1538c174e81aad04d706
EcksDeee <36055694+EcksDeee@users.noreply.github.com>
2018-05-14 22:02:38 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,7 +32,7 @@ client.on('message', msg => {
         } else if (command === 'noscope') {
             msg.channel.send("", {file: "https://media2.giphy.com/media/UJLtz0z7VrAIM/200.gif"});
         } else if (command === 'help') {
-            msg.channel.send("ReverseTrap [Owner: F1R3SHAD0W]\nBeta\n:>help: Lists possible commands\n:>ping: Says pong..What did u expect??\n:>gifhelp: Lists possible gifs\n:>status: Not Ready\n:>play [Music] W.I.P");
+            msg.channel.send("ReverseTrap [Owner: F1R3SHAD0W]\nBeta\n:>help: Lists possible commands\n:>ping: Says pong..What did u expect??\n:>gifhelp: Lists possible gifs\n:>status: Not Ready\n:>musicCMDS");
         } else if (command === 'stickman') {
             msg.channel.send("", {file: "https://media0.giphy.com/media/jSXkVsYaQxw6k/200.gif"});
         } else if (command === 'rekt') {
